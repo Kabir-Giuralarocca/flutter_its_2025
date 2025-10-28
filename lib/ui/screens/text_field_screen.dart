c5084bd27b954a38b6849d7fa220a5d7443a236a
Kabir Giuralarocca <giuralaroccakabir99@gmail.com>
2025-10-28 12:51:03 +0100

    Add date selection functionality to TextFieldScreen and include intl package

--- lib/ui/screens/text_field_screen.dart
+++ lib/ui/screens/text_field_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class TextFieldScreen extends StatefulWidget {
   const TextFieldScreen({super.key});
@@ -10,6 +11,21 @@ class TextFieldScreen extends StatefulWidget {
 
 class _TextFieldScreenState extends State<TextFieldScreen> {
   bool _obscureText = true;
+  final TextEditingController _dateController = TextEditingController(text: '01/01/2025');
+  final DateFormat _formatter = DateFormat.yMd();
+
+  void _selectDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      setState(() {
+        _dateController.text = _formatter.format(picked);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +71,22 @@ class _TextFieldScreenState extends State<TextFieldScreen> {
               keyboardType: TextInputType.number,
               obscureText: _obscureText,
               obscuringCharacter: '*',
+            ),
+            TextField(
+              keyboardType: TextInputType.datetime,
+              controller: _dateController,
+              decoration: InputDecoration(
+                labelText: 'Date',
+                hintText: 'DD/MM/YYYY',
+                filled: true,
+                fillColor: Colors.blueGrey.shade100,
+                contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(32), gapPadding: 2),
+                suffixIcon: IconButton(
+                  onPressed: _selectDate,
+                  icon: const Icon(Icons.calendar_today),
+                ),
+              ),
             ),
           ],
         ),
